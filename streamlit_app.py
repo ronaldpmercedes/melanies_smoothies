7d90c6d5a96b6d3d1e9d7fe0321e2c3cafbe5d00
ronaldpmercedes <ronald.p.mercedes@accenture.com>
2024-10-11 15:33:58 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,14 +17,13 @@ st.dataframe(data=my_dataframe, use_container_width=True)
 ingredients_list = st.multiselect ( 'Choose up to 5 ingredients: ', my_dataframe )
 
 if ingredients_list:
-
-
-    ingredients_string = ' '
-
+    ingredients_string = ''
     for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen
-
-   # st.write(ingredients_string)
+        ingredient_string += fruit_chosen + ''
+        fruityvice_response =  requests.get("https://fruityvice.com/api/fruit/watermelon")
+        st.subheader(fruit_chosen, 'Nutrition Infomatio')
+        fruityvice_response = requests.get ("https://fruityvice.com/api/fruit" + fruit_chosen)
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
             values ('""" + ingredients_string + """')"""
@@ -41,13 +40,3 @@ if ingredients_list:
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
-
-if ingrediants_list:
-        ingrediant_string = ''
-    
-        for fruit_chosen in ingredient_list:
-            ingredient_string += fruit_chosen + ' '
-            fruityvice_response =  requests.get("https://fruityvice.com/api/fruit/watermelon")
-            st.subheader(fruit_chosen, 'Nutrition Infomatio')
-            fruityvice_response = requests.get ("https://fruityvice.com/api/fruit" + fruit_chosen)
-            fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
