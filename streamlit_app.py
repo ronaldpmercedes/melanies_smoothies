cb28b5cf0c6ba55c17e631ac70e34fb67ee68e1f
ronaldpmercedes <ronald.p.mercedes@accenture.com>
2024-10-11 13:43:58 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 # Import python packages
 import streamlit as st
 # from snowflake.snowpark.context import get_active_session
-#import snowflake.snowpark.functions import col
+import snowflake.snowpark.functions import col
 # Write directly to the app
 st.title(":cup_with_straw: Customize your Smoothie :cup_with_straw:")
 st.write(
