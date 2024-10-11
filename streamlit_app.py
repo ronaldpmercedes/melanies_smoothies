c697053d4d6c5e50b7c504ccfe7af59841613fd2
ronaldpmercedes <ronald.p.mercedes@accenture.com>
2024-10-11 15:50:05 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,11 @@ session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
-ingredients_list = st.multiselect ( 'Choose up to 5 ingredients: ', my_dataframe, ,max_selections =5 )
+ingredients_list = st.multiselect ( 
+    'Choose up to 5 ingredients: '
+    ,my_dataframe 
+    ,max_selections =5 
+    )
 
 if ingredients_list:
     ingredients_string = ''
