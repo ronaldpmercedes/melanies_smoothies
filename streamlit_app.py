a8c09fa260711184286811cd87166d008d6c818f
ronaldpmercedes <ronald.p.mercedes@accenture.com>
2024-10-11 14:52:35 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,8 +38,7 @@ if ingredients_list:
     
         st.success('Your Smoothie is ordered!', icon="✅")
 
-
-
+#new section to display
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 st.text(fruityvice_response.json())
