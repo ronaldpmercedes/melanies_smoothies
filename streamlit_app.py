8c0a2270d4d94b44799d921800a0945e041c31a8
ronaldpmercedes <ronald.p.mercedes@accenture.com>
2024-10-12 14:49:29 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,20 +21,24 @@ st.dataframe(data=my_dataframe, use_container_width=True)
 ingredients_list = st.multiselect ( 'Choose up to 5 ingredients: ',my_dataframe ,max_selections =5 )
 
 if ingredients_list:
-    st.write(ingredients_list)
-    st.text(ingredients_list)
-    
+    #st.write(ingredients_list)
+    #st.text(ingredients_list)
     ingredients_string = ''
     
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        st.subheader(fruit_chosen + 'Nutrition Information')
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=TRUE)
         
-    #    my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
-    #    values ('""" + ingredients_string + """')"""
+
+        #search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        
+        
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
 
-   # st.write(my_insert_stmt)
+
     time_to_insert = st.button('Submit Order')
     
     if time_to_insert:
