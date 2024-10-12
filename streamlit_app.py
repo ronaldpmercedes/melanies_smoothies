f0853ca64cbd2b6315c35e14337a1af9ade090b3
ronaldpmercedes <ronald.p.mercedes@accenture.com>
2024-10-12 14:56:21 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,8 +15,9 @@ name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
 
 #session = get_active_session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop
 
 ingredients_list = st.multiselect ( 'Choose up to 5 ingredients: ',my_dataframe ,max_selections =5 )
 
@@ -27,9 +28,7 @@ if ingredients_list:
     
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-        st.subheader(fruit_chosen + 'Nutrition Information')
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
-        fv_df = st.dataframe(data=fruityvice_response.csv(), use_container_width=TRUE)
+
         
 
         #search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
